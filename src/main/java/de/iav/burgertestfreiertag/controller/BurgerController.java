af086bf39847a4b656502e046d5c32fda0035eb8
Dirk Stadge <dirk_stadge@yahoo.de>
2023-07-13 15:34:51 +0200

    Update einiger Dateien

--- src/main/java/de/iav/burgertestfreiertag/controller/BurgerController.java
+++ src/main/java/de/iav/burgertestfreiertag/controller/BurgerController.java
@@ -2,9 +2,7 @@ package de.iav.burgertestfreiertag.controller;
 
 import de.iav.burgertestfreiertag.model.Menu;
 import de.iav.burgertestfreiertag.service.BurgerService;
-
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -22,16 +20,13 @@ public class BurgerController {
     public List<Menu> getManuBoard() {
         return burgerService.getMenuBoard();
     }
-
     @GetMapping("/menus/{id}")
     public Menu getMenuById(@PathVariable int id) {
         return burgerService.getMenuById(id);
     }
-
     @PutMapping("/menus/{id}")
     public Menu changeMenu(@PathVariable int id, @RequestBody Menu newMenu){
         return burgerService.changeMenu(id, newMenu);
-
     }
     @PostMapping("/menus")
     public void addMenu(@RequestBody Menu menuToAdd){
